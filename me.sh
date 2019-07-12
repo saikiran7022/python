f8b5a5bed327a32c83825f7087c217725b5728a8
saikiran7022 <saikiranlingadally@gmail.com>
2019-07-12 15:43:33 +0530

    done me.sh

--- me.sh
+++ me.sh
@@ -2,7 +2,7 @@
 git init
 git add ${1}
 git commit -m "done ${1}"
-git remote add origin3 ${2}
+git remote add ${3} ${2}
 git status
-git push -u origin3 new_branch
+git push -u ${3} ${4}
 git  remote -v
